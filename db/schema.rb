2203aa1e03ad4e2968d70a045939d8bda7688646
luckeciano <luckeciano@hotmail.com>
2016-05-22 02:02:13 +0000

    completando user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522014514) do
+ActiveRecord::Schema.define(version: 20160522020119) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 20160522014514) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "name"
+    t.string   "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
