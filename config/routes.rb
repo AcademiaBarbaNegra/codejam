4b90ab151f491c60c48b42d0bd8810748b8371f4
mknarciso <murilokn@gmail.com>
2016-05-22 04:19:01 +0000

    Login/ Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :orgs
   #devise_for :users, controllers: { sessions: "users/sessions" }
   devise_for :users, :controllers => {:registrations => "users/registrations"}
- 
+  get 'users' => 'users#index'
+  get 'users/:id' => 'users#show', as: :user
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
