0e24231f0e8283ec19ae723b8c0abe844f6b657a
adautobraz <adautobraz.neto@gmail.com>
2016-05-22 08:13:54 +0000

    Courses layout ok

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
     has_attached_file :picture, styles: { small: "64x64#", square: "200x200#", large: "600x600#"}
-    has_one :org
+    has_attached_file :cover, styles: { cover: "800x300#"}
+    belongs_to :org
     belongs_to :owner, :class_name => "User" , primary_key: "owner_id"
     has_many :user_courses
     has_many :users, :through => :user_courses
