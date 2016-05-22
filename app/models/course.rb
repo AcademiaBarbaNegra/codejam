dbe241d8654f1df2295ef325d64342fb86bcb43e
luckeciano <luckeciano@hotmail.com>
2016-05-22 03:31:11 +0000

    alteração em tabelas

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
+    has_attached_file :picture
     belongs_to :org
     belongs_to :owner, :class_name => "User" , primary_key: "owner_id"
     has_many :user_courses
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522022114) do
+ActiveRecord::Schema.define(version: 20160522032924) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160522022114) do
     t.datetime "updated_at",   null: false
     t.integer  "org_id"
     t.integer  "owner_id"
+    t.datetime "start"
+    t.datetime "end"
+    t.float    "price"
+    t.float    "grade"
+    t.string   "level"
+    t.float    "coursegrade"
   end
 
   add_index "courses", ["org_id"], name: "index_courses_on_org_id"
@@ -92,6 +98,7 @@ ActiveRecord::Schema.define(version: 20160522022114) do
     t.datetime "photo_updated_at"
     t.string   "name"
     t.string   "bio"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
